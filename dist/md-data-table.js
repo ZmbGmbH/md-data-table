536b08023c1a9f2ce7b8ab88ca41baf235a8a922
[ZMB] Jovan Gerodetti <jovan@zmbcore.com>
2018-01-15 16:41:39 +0100

    dist update

--- dist/md-data-table.js
+++ dist/md-data-table.js
@@ -104,59 +104,59 @@ function mdBody() {
 angular.module('md.data.table').directive('mdCell', mdCell);
 
 function mdCell() {
-  
+
   function compile(tElement) {
     var select = tElement.find('md-select');
-    
+
     if(select.length) {
       select.addClass('md-table-select').attr('md-container-class', 'md-table-select');
     }
-    
+
     tElement.addClass('md-cell');
-    
+
     return postLink;
   }
-  
+
   // empty controller to be bind properties to in postLink function
   function Controller() {
-    
+
   }
-  
+
   function postLink(scope, element, attrs, ctrls) {
     var select = element.find('md-select');
     var cellCtrl = ctrls.shift();
     var tableCtrl = ctrls.shift();
-    
+
     if(attrs.ngClick) {
       element.addClass('md-clickable');
     }
-    
+
     if(select.length) {
       select.on('click', function (event) {
         event.stopPropagation();
       });
-      
+
       element.addClass('md-clickable').on('click', function (event) {
         event.stopPropagation();
         select[0].click();
       });
     }
-    
+
     cellCtrl.getTable = tableCtrl.getElement;
-    
+
     function getColumn() {
       return tableCtrl.$$columns[getIndex()];
     }
-    
+
     function getIndex() {
-      return Array.prototype.indexOf.call(element.parent().children(), element[0]);
+      return Array.prototype.indexOf.call(element[0].parentElement.children, element[0]);
     }
-    
+
     scope.$watch(getColumn, function (column) {
       if(!column) {
         return;
       }
-      
+
       if(column.numeric) {
         element.addClass('md-numeric');
       } else {
@@ -164,7 +164,7 @@ function mdCell() {
       }
     });
   }
-  
+
   return {
     controller: Controller,
     compile: compile,
@@ -172,6 +172,7 @@ function mdCell() {
     restrict: 'A'
   };
 }
+
 
 angular.module('md.data.table').directive('mdColumn', mdColumn);
 
